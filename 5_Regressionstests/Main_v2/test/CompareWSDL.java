87201970e4d3601c875f8ab741595ed2721f653c
mostermann96 <mostermann96@web.de>
2020-01-29 12:52:43 +0100

    added soa jar, modified WSDLcompare

--- 5_Regressionstests/Main_v2/test/CompareWSDL.java
+++ 5_Regressionstests/Main_v2/test/CompareWSDL.java
@@ -1,21 +1,27 @@
-
 import java.util.List;
 import com.predic8.wsdl.*;
 import com.predic8.wsdl.diff.WsdlDiffGenerator;
 import com.predic8.soamodel.Difference;
 
 public class CompareWSDL {
+    String cpOrig;
+    String cpMod;
+    String output;
 
-    public static void main(String[] args) {
-        compare();
+    public CompareWSDL(String cpOrig, String cpMod, String output){
+        this.cpMod=cpMod;
+        this.cpOrig=cpOrig;
+        this.output=output;
+
+
     }
 
-    private static void compare(){
+    private static void compare(String cpOrig,String cpMod){
         WSDLParser parser = new WSDLParser();
 
-        Definitions wsdl1 = parser.parse("resources/diff/1/article.wsdl");
+        Definitions wsdl1 = parser.parse(cpOrig);
 
-        Definitions wsdl2 = parser.parse("resources/diff/2/article.wsdl");
+        Definitions wsdl2 = parser.parse(cpMod);
 
         WsdlDiffGenerator diffGen = new WsdlDiffGenerator(wsdl1, wsdl2);
         List<Difference> lst = diffGen.compare();
